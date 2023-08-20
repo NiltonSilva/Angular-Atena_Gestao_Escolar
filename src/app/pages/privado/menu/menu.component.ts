af554b1d65d38c343958f8273ccf0f883e6ffb05
Nilton S Ferreira <nsfgeo@hotmail.com>
2023-08-20 18:12:58 -0300

    feat: aplicado transição na rotação das setas do menu.

--- src/app/pages/privado/menu/menu.component.ts
+++ src/app/pages/privado/menu/menu.component.ts
@@ -15,29 +15,32 @@ export class MenuComponent implements OnInit {
   ngOnInit(): void { }
 
   encolherOuExpandirSubMenu(event: Event) {
-    this.encolherTodosOsSubMenus(event);
-
     let target: any = event.currentTarget;
     let arrow: any = target?.children[0]?.children[2]?.firstChild
 
     target.children[1]?.classList.toggle('show');
 
-    if (target.children[1]?.classList.contains('show')) {
+    if (target?.children[1]?.classList.contains('show')) {
       arrow.style.transform = "rotate(90deg)";
+      arrow.style.transition = "0.3";
     } else {
       arrow.style.transform = "rotate(0deg)";
+      arrow.style.transition = "0.3s";
     }
+    this.encolherTodosOsSubMenus(event);
+
   }
 
   encolherTodosOsSubMenus(event: Event) {
-
     let target: any = event.currentTarget;
     let menu: any = target.parentElement.children;
     let listaMenu: any = [...menu];
 
     for (let i = 0; i < listaMenu.length; i++) {
-      if (listaMenu[i]?.children[1]) {
-        listaMenu[i]?.children[1].classList.add('none');
+      if (listaMenu[i]?.children[1]?.classList.contains('show')) {
+        console.log('oi')
+      } else {
+        listaMenu[i]?.children[1]?.classList.add('none')
       }
     }
   }
